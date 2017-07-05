f612cc52faa8ea31ac257b60dfb8bfd4f77dfdd6
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-07-05 21:25:00 +0200

    add center button

--- src/ConnectedMap.js
+++ src/ConnectedMap.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
 
 const User = ({ position }) => {
@@ -17,20 +17,36 @@ const Map = ReactMapboxGl({
   accessToken: ''
 });
 
-const ConnectedMap = ({ bearing, center }) => {
-  console.log(center());
+class ConnectedMap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      map: null
+    };
+  }
   /*eslint-disable react/style-prop-object*/
-  return (
-    <Map
-      style="https://openmaptiles.github.io/positron-gl-style/style-cdn.json"
-      containerStyle={{ height: '70vh', width: '100vw' }}
-      zoom={[13]}
-      center={center()}
-      bearing={bearing}
-    >
-      <User position={center()} />
-    </Map>
-  );
-};
+  render() {
+    const { bearing, center } = this.props;
+    return (
+      <div>
+        <Map
+          style="https://openmaptiles.github.io/positron-gl-style/style-cdn.json"
+          containerStyle={{ height: '70vh', width: '100vw' }}
+          zoom={[13]}
+          center={center()}
+          bearing={bearing}
+          onStyleLoad={map => this.setState({ map })}
+        >
+          <User position={center()} />
+        </Map>
+        <button onClick={() => this.state.map.flyTo({ center: center() })}>
+          CENTER
+        </button>
+      </div>
+    );
+  }
+  /*eslint-enable react/style-prop-object*/
+}
 
 export default ConnectedMap;
