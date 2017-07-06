2e6cbd1b90d1976e3bd2a417d601b068073d1e3b
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-07-06 13:53:37 +0200

    added close icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,11 @@
 import NavYellow from './marker-yellow.svg';
 import NavWhite from './marker-white.svg';
 import NavBlue from './marker-blue.svg';
+import Close from './close.svg';
 
 export default {
   NavYellow,
   NavWhite,
-  NavBlue
+  NavBlue,
+  Close
 };
