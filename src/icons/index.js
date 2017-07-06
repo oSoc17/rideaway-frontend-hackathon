7fc24a42721cc93e543ac504ad681ce019d5e359
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-07-06 11:28:03 +0200

    add icons

---
+++ src/icons/index.js
@@ -0,0 +1,9 @@
+import NavYellow from './marker-yellow.svg';
+import NavWhite from './marker-white.svg';
+import NavBlue from './marker-blue.svg';
+
+export default {
+  NavYellow,
+  NavWhite,
+  NavBlue
+};
