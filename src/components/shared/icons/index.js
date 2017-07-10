c31d871ce1d28b955cdcf36ba7e5693f5343c426
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-07-10 14:34:50 +0200

    add icons

--- src/components/shared/icons/index.js
+++ src/components/shared/icons/index.js
@@ -1,11 +1,21 @@
+import Hamburger from './hamburger.svg';
 import NavYellow from './marker-yellow.svg';
 import NavWhite from './marker-white.svg';
 import NavBlue from './marker-blue.svg';
 import Close from './close.svg';
+import Discover from './discover-empty.svg';
+import Navigate from './navigate-empty.svg';
+import DiscoverFilled from './discover-full.svg';
+import NavigateFilled from './navigate-full.svg';
 
 export default {
+  Hamburger,
   NavYellow,
   NavWhite,
   NavBlue,
-  Close
+  Close,
+  Discover,
+  Navigate,
+  DiscoverFilled,
+  NavigateFilled
 };
